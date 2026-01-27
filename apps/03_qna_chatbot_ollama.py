fd77b042a4be4a29a50ed5acc90ac2e6d6f0aca6
Rushikesh Borkar <borkarananta028@gmail.com>
2026-01-27 11:27:32 +0530

    added ollama bot with memory

---
+++ apps/03_qna_chatbot_ollama.py
@@ -0,0 +1,30 @@
+from langchain_ollama import ChatOllama
+import streamlit as st
+
+llm = ChatOllama(model="gemma3:1b")
+
+st.title("ChatBuddy - AI Assistant at your Service.")
+st.markdown("Chatbot created using ollama.")
+
+if "memory" not in st.session_state:
+    st.session_state.memory = []
+
+for entry in st.session_state.memory:
+    st.chat_message(entry["role"]).markdown(entry["content"])
+
+
+query = st.chat_input("Ask anything to gemma...")
+
+if query:
+    st.chat_message("user").markdown(query)
+    st.session_state.memory.append({"role": "user", "content": query})
+
+    with st.spinner("Thinking..."):
+        res = llm.invoke(st.session_state.memory)
+
+    st.balloons()
+
+    st.chat_message("ai").markdown(res.content)
+    st.session_state.memory.append({"role": "ai", "content": res.content})
+
+    
